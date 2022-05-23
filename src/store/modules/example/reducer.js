6f73061e1268abfbf9dea3d4032733c0b270ef2b
Guilherme Almeida <guimathiasgui@gmail.com>
2022-05-22 22:23:27 -0300

    Setting redux saga

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,13 +1,26 @@
+import * as types from '../types';
+
 const initialState = {
   buttonClicked: false,
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'BUTTON_CLICKED': {
+    case types.BUTTON_CLICKED_SUCCESS: {
+      console.log('Success');
       const newState = { ...state };
       newState.buttonClicked = !newState.buttonClicked;
       return newState;
+    }
+
+    case types.BUTTON_CLICKED_FAILURE: {
+      console.log('Failure');
+      return state;
+    }
+
+    case types.BUTTON_CLICKED_REQUEST: {
+      console.log('Making request');
+      return state;
     }
 
     default: {
---
+++ src/store/modules/example/sagas.js
@@ -0,0 +1,24 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import * as actions from './actions';
+import * as types from '../types';
+
+const request = () =>
+  // new Promise((resolve, reject) => {
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+
+function* exempleRequest() {
+  try {
+    yield call(request);
+    yield put(actions.clickButtonSuccess());
+  } catch {
+    toast.error('Error');
+    yield put(actions.clickButtonFailure());
+  }
+}
+
+export default all([takeLatest(types.BUTTON_CLICKED_REQUEST, exempleRequest)]);
